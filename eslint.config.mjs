62521ea3e27650fb425aa8b40d7cb6ab36a80e10
Mike <mikethomasmobile@gmail.com>
2026-02-07 20:36:06 +0000

    Hmmm

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   {
-    ignores: [".next/**", "node_modules/**", "public/**"],
+    ignores: [".next/**", "out/**", "node_modules/**", "public/**"],
   },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ]
